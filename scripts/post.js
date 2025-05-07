df881b527d2a2b3b399ca34d3b4091b71c7f76cf
mindsers <nathanael@cherrier.dev>
2025-05-07 16:18:13 +0200

    Fix shiki script for syntax highligting

--- scripts/post.js
+++ scripts/post.js
@@ -1,20 +1,18 @@
+import { codeToHtml } from 'https://esm.sh/shiki@3.0.0'
 import { ready } from './ready.js'
 
 const isDark =
-  window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-const highlighter = shiki.getHighlighter({
-  theme: isDark ? 'github-light' : 'github-dark',
-})
+  window.matchMedia?.('(prefers-color-scheme: dark)').matches
 
 ready(async () => {
   const images = document.querySelectorAll('.kg-gallery-image img')
-  images.forEach(function (image) {
+  images.forEach((image) => {
     const container = image.closest('.kg-gallery-image')
     const width = image.attributes.width.value
     const height = image.attributes.height.value
     const ratio = width / height
 
-    container.style.flex = ratio + ' 1 0%'
+    container.style.flex = `${ratio} 1 0%`
   })
 
   const videos = document.querySelectorAll('iframe[src*=youtube]')
@@ -27,8 +25,7 @@ ready(async () => {
   const codeblocks = document.querySelectorAll('pre > code[class^="language-"]')
   for (const block of codeblocks) {
     const [, language] = Array.from(block.classList)[0].split('-')
-
-    const code = (await highlighter).codeToHtml(block.innerText, language)
+    const code = await codeToHtml(block.innerText, {lang : language, theme: isDark ? 'github-light' : 'github-dark'})
 
     block.parentNode.outerHTML = code
   }
